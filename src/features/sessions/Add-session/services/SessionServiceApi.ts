a564a27fc9f1fa8b504f7e83488496051dd49cdd
shahdnajjar <shahdnajjar50@gmail.com>
2025-07-10 10:15:18 +0100

    sessiontype v2

--- src/features/sessions/Add-session/services/SessionServiceApi.ts
+++ src/features/sessions/Add-session/services/SessionServiceApi.ts
@@ -15,16 +15,11 @@ export type SessionResponse = {
 const SessionServiceApi = {
   create: async (payload: SessionFormData): Promise<SessionResponse> => {
     try {
-      // Transform payload to match backend expectations
-      const { categoryId, sessionSchedules, ...rest } = payload;
+      const { categoryId, sessionSchedules = [], ...rest } = payload;
       const transformedPayload = {
         ...rest,
-        sessionSchedules: sessionSchedules.map((schedule) => ({
-          ...schedule,
-          sessionType: payload.sessionType, // Add sessionType to each schedule
-        })),
+        sessionSchedules: [...sessionSchedules], 
       };
-
       const response = await axiosInstance.post<SessionResponse>(
         `/api/v1/sessions`,
         transformedPayload
